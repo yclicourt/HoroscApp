9266f1c185862bd3dcaabef5f2bd6a18ab58185e
Yoan <yclicourt@gmail.com>
2023-12-07 22:37:58 -0500

    initial commit

---
+++ app/src/main/java/com/example/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,26 @@
+package com.example.horoscapp.ui.horoscope
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.horoscapp.databinding.FragmentHoroscBinding
+
+
+class HoroscopeFragment : Fragment() {
+
+
+    private var _binding: FragmentHoroscBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHoroscBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+}
